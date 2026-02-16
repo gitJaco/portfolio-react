dcaea0e23095eb2b990994fd270a8bfb703e2e8f
Jacobo <jacopeth88@gmail.com>
2026-02-15 18:00:42 -0600

    Arreglar background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,10 @@
 import profile from "../../public/profile.jpg"
+import bg from "../assets/background.jpg"
 
 export default function Hero() {
   return (
     <header className="hero hero-image">
-      <div className="hero-overlay">
+      <div className="hero-overlay" style={{ backgroundImage: `url(${bg})`, background: 'rgba(2, 6, 23, 0.75)'}}>
         <img src={profile} alt="Jacobo De Hoyos" className="avatar"></img>
         <h1 className="fade-in">Jacobo De Hoyos Reyes</h1>
         <p className="fade-in delay">
